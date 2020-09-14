67729fa0633a9651f2d3dae26ff5c955355c8a50
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-14 12:30:21 +0700

    init test space

---
+++ build/precache-manifest.16649cf40fc19604ec7d8d35c30f9104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2326e84ea6474c29c7ee8e074834da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac2f62ee08ecbe6b242",
+    "url": "/static/css/main.104ad7cb.chunk.css"
+  },
+  {
+    "revision": "1f9feedfe982cb157381",
+    "url": "/static/js/2.662edce1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.662edce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac2f62ee08ecbe6b242",
+    "url": "/static/js/main.0f974143.chunk.js"
+  },
+  {
+    "revision": "17689816fef00367a73a",
+    "url": "/static/js/runtime-main.c8a81e10.js"
+  }
+]);
